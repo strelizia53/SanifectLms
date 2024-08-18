e62307cc29e0fbe1303ed6befe7eba20dc9761cc
Strelizia <161130212+strelizia53@users.noreply.github.com>
2024-08-18 09:13:26 +0530

    adding landing page base and edited web

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sanifect</title>
+</head>
+<body>
+    <h1>The landing page for the sanifect lms</h1>
+    <a href="#">Click here to login</a>
+    <br>
+    <a href="#">Click here to register</a>
+</body>
+</html>
